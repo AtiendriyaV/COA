625a84de64629fad6ff06ea06d18a7b6943903c9
AtiendriyaV <65350839+AtiendriyaV@users.noreply.github.com>
2022-11-23 09:32:57 +0530

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -1,6 +1,4 @@
-  //declation of header files 
-  
-  #include <stdio,h>
+  #include <stdio.h>
   #include <stdlib.h>
   #include <omp.h>
   #include <sys/time.h>
@@ -10,26 +8,29 @@
 
   //declaration of variable for the matrix  A and B  for output in C matrix
 
-   int A[N][N]; ,B[N][N];
-   int C[N][N];
+   int A[N][N] , B[N][N] , C[N][N];
    
-   int main(){
+   int main()
+   {
+   int i,j,k=0;
+   
    
    // structure for time interval and time zone
     
    struct timeval tv1,tv2;
+   struct timezone tz;
 
    double elapsed;  //time assosiated with particular time
     
    // declation of omp for number of threads allocated for this code and to get the number of processors
         
-   omp_set_num_threads(omp_get_num_proc());
+   omp_set_num_threads(omp_get_num_procs());
    
    //giving value to the A and B matrix
    
-   for(i=0;i<3;i++); //for matrix of A matrix of size 3
+   for(int i=0;i<3;i++); //for matrix of A matrix of size 3
    { 
-      for(j=0;j<3;j++); //for matrix of B matrix of size 3
+      for(int j=0;j<3;j++); //for matrix of B matrix of size 3
      {
    //provide identical matrix of 3*3
    A[i][j] = 2; //for value like A11 , A12 ...........
@@ -40,13 +41,13 @@
    }
     }  
    
-    gettimeofday(&tv1); 
+    gettimeofday(&tv1,&tz); 
    //declation of parallel processing and multiplying the matrix
    
    #pragma omp parallel for private(i,j,k) shared (A,B,C)
    for (int i = 0; i < 3; ++i)  // first it will increament the valure of i then it will process in for loop
     {
-    print(i);
+    printf("%d\n",i);
     
       for (int j = 0; j < 3; ++j)
        {
@@ -59,7 +60,7 @@
    
    //declation of end time and formula for elapsed time = end time - start time
    
-   gettimeofday(&tv2);
+   gettimeofday(&tv2, &tz);
    elapsed = (double)(tv2.tv_sec-tv1.tv_sec)+(double)
    
    (tv2.tv_usec-tv1.tv_usec)*1.e-6;
@@ -71,7 +72,6 @@
     {
     printf("%d\t" , C[i][j]);
     }
-    printf("\n")
+    printf("\n");
    } 
   }
-        
